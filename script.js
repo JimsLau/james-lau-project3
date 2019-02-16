abff05168593a7b9fca3bd23ac15ec31c8bdb9bc
James <james83lau@gmail.com>
2019-02-16 14:27:31 -0500

    added description

--- script.js
+++ script.js
@@ -1,73 +1,97 @@
 const zodiacInfo = [
 	{
-		name : 'rat',
+		name : 'Rat',
 		image: 'assets/Rat.png',
-		description: "Rat-forthright, generous, outgoing, loves money, hates waste"
+		description: " Rats are quick-witted, resourceful, versatile, kind, smart, and lovely. With strong intuition and quick responses, Rats always easily adapt themselves to a new environment. With rich imaginations and sharp observations, they can take advantage of various opportunities well. Rats have strong curiosity, so they tend to try their hands at anything, and they can deal with tasks skillfully. ",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'ox',
+		name: 'Ox',
 		image: 'assets/Ox.png',
-		description: "Ox-calm, dependable, obstinate, reliable, proud, and can be uncompromising"
+		description: " Oxes are known for diligence, dependability, strength and determination. Having an honest nature, Oxes are strongly patriotic, have ideals and ambitions for life, and attach importance to family and work. These reflect traditional conservative characteristics. Having great patience and a desire to make progress, Oxes can achieve their goals by consistent effort. They are not much influenced by others or the environment, but persist in doing things according to their ideals and capabilities. Before taking any action, Oxes will have a definite plan with detailed steps, to which they apply their strong faith and physical strength. As a result, people of the Ox zodiac sign often enjoy great success. Oxes are weakest in their communication skills. They are not good at communicating with others, and even think it is not worthwhile to exchange ideas with others. They are stubborn and stick to their own ways.  ",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'tiger',
+		name: 'Tiger',
 		image: 'assets/Tiger.png',
-		description: "Tiger-loving, giving, optimistic, idealistic, stubborn, self-centered, emotional"
+		description: "Tigers are brave, competitive, unpredictable, and confident. They are very charming and well-liked by others. But sometimes they are likely to be impulsive, bad-tempered, and stubborn. With stubborn personalities and tough judgment, Tigers work actively and express themselves boldly, doing things in a high-handed manner. They are authoritative and never go back on what they have said. With great confidence and indomitable fortitude, they can be competent leaders. They will not make preparations for anything, but can handle anything that comes along. ",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'rabbit',
+		name: 'Rabbit',
 		image: 'assets/Rabbit.png',
-		description: " Rabbit-careful, systematic, considerate, can be indifferent, temperamental, shrewd"
+		description: "Rabbits tend to be gentle, quiet, elegant, and alert as well as quick, skillful, kind, patient, and very responsible. However, they might be superficial, stubborn, and overly-discreet. Rabbits are faithful to those around them but are reluctant to reveal their minds to others, and have a tendency to escape reality. They are too cautious and conservative, which means they miss good opportunities.",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'dragon',
+		name: 'Dragon',
 		image: 'assets/Dragon.png',
-		description: "Dragon- strong, energetic, proud, confident, but can be illogical and obsessive. Read about the dragon boat festival "
+		description: "Among the Chinese zodiac animals, the dragon is the sole imaginary animal. The Chinese dragon is the most vital and powerful beast in the Chinese zodiac, although Dragons have an infamous reputation for being a hothead and possessing a sharp tongue. In ancient times, people thought that Dragons were best suited to be leaders of the world with their character traits of dominance and ambition. Gifted with innate courage, tenacity, and intelligence, Dragons are enthusiastic and confident. They are not afraid of challenges, and willing to take risks. However, Dragons are sometimes regarded as aggressive, and angry Dragons are not open to criticism. They don't consider themselves irritating and arrogant. Instead of following tradition, they strive for a smooth future.",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'snake',
+		name: 'Snake',
 		image: 'assets/Snake.png',
-		description: "Snake-intellectual, superstitious, independent, private, cautious"
+		description: " In Chinese culture, the Snake is the most enigmatic animal among the 12 zodiac animals. People born in a year of the Snake are supposed to be the most intuitive. Snakes tend to act according to their own judgments while remaining private and reticent. They are determined to accomplish their goals and hate to fail. Snakes represent the symbol of wisdom. They are intelligent and wise. They are good at communication but say little. Snakes are usually regarded as great thinkers. They love to possess the best of everything but they have no patience for shopping. Snake people prefer to work alone; therefore, they are easily stressed. If they seem unusually stressed, it is best to allow them their own space and time to return to normal. ",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'horse',
+		name: 'Horse',
 		image: 'assets/Horse.png',
-		description: "Horse-cheerful, lively, impulsive, manipulative, friendly, self-reliant "
+		description: " People born in a year of the Horse are extremely animated, active and energetic. Horses love to be in a crowd, and they can usually be seen on such occasions as concerts, theater performances, meetings, sporting events, and parties. With a deft sense of humor, Horses are masters of repartee. They love to take center stage and delight audiences everywhere.  ",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'sheep',
+		name: 'Sheep',
 		image: 'assets/Sheep.png',
-		description: "Sheep- good-natured, timid, emotional, pessimistic, mild, forgiving"
+		description: " People born in a year of the Sheep are generally believed to be gentle, mild-mannered, shy, stable, sympathetic, amicable, and brimming with a strong sense of kindheartedness and justice. They have very delicate thoughts, strong creativity, and perseverance, and acquire professional skills well. Although they look gentle on the surface, they are tough on the inside, always insisting on their own opinions in their minds. They have strong inner resilience and excellent defensive instincts. Although they prefer to be in groups, they do not want to be the center of attention. They are reserved and quiet, most likely because they like spending a lot of time in their own thoughts. Sheeps like to spend money on fashionable things that give them a first class appearance. Although Sheeps enjoy spending money on the finer things in life, they are not snobbish. ",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'monkey',
+		name: 'Monkey',
 		image: 'assets/Monkey.png',
-		description: "Monkey-successful, charming, crafty, can be dishonest, self-centered, inquisitive "
+		description: " People born in a year of the Monkey have magnetic personalities and are witty and intelligent. Personality traits like mischievousness, curiosity, and cleverness make them very naughty. Monkeys are masters of practical jokes, because they like playing most of the time. Although they don't have bad intentions, their pranks sometimes hurt other people's feelings. Monkeys are fast learners and crafty opportunists. They have many interests and need partners who are capable of stimulating them. While some like the eccentric nature of Monkeys, others don't trust their sly, restless, and inquisitive nature. Although they are clever and creative, Monkeys can't always exhibit their talents properly. They like to accept challenges and prefer urban life to rural life. ",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'rooster',
+		name: 'Rooster',
 		image: 'assets/Rooster.png',
-		description: "Rooster-conservative, aggressive, decisive, logical, can be overly critical"
+		description: " People born in a year of the Rooster are very observant. Hardworking, resourceful, courageous, and talented, Roosters are very confident in themselves. Roosters are always active, amusing, and popular within a crowd. Roosters are talkative, outspoken, frank, open, honest, and loyal individuals. They like to be the center of attention and always appear attractive and beautiful. Roosters are happiest when they are surrounded by others, whether at a party or just a social gathering. They enjoy the spotlight and will exhibit their charm on any occasion. ",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'dog',
+		name: 'Dog',
 		image: 'assets/Dog.png',
-		description: "Dog-clever, willing to help others, open-minded, practical, can be belligerent "
+		description: " Dogs are loyal and honest, amiable and kind, cautious and prudent. Due to having a strong sense of loyalty and sincerity, Dogs will do everything for the person who they think is most important. As Dogs are not good at communication, it is difficult for them to convey their thoughts to others. Therefore, Dogs tend to leave others with the impression that they have a stubborn personality. Born with a good nature, Dogs do not tend to be criminals or seek dishonest gains. They just need a quiet life and a good family and, therefore, forget the ugliness in the world. Dogs are always ready to help others and do not care about their own interests; but, if they find themselves betrayed by cunning people they will feel shocked and hurt. When thrown into doubt, Dogs think the world is evil and complicated. Then they criticize sharply when making comments, and infer that all things are according to their pessimistic point of view.  ",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	{
-		name: 'pig',
+		name: 'Pig',
 		image: 'assets/Pig.png',
-		description: "Pig-zhū brave, trustworthy, patient, diplomatic, can be hot-tempered"
+		description: " Pigs are diligent, compassionate, and generous. They have great concentration: once they set a goal, they will devote all their energy to achieving it. Though Pigs rarely seek help from others, they will not refuse to give others a hand. Pigs never suspect trickery, so they are easily fooled. General speaking, Pigs are relatively calm when facing trouble. No matter how difficult the problems are, they can handle things properly and carefully. They have a great sense of responsibility to finish what they are engaged in. ",
+		luckyThings: "",
+		unluckyThings: ""
 	},
 	
 ]
 
-let zodiacAnimals = ['monkey','rooster','dog', 'pig', 'rat', 'ox', 'tiger', 'rabbit', 'dragon', 'snake', 'horse', 'sheep'];
+const animals = ['Monkey','Rooster','Dog', 'Pig', 'Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Sheep'];
 
 function getAnimalName(year) {
 	if(year) {
-		let animalIndex = year % 12;
-		return zodiacAnimals[animalIndex];
+		const animalIndex = year % 12;
+		return animals[animalIndex];
 	}
 	
 	return "";
@@ -88,22 +112,22 @@ function getAnimalInformation(animalName) {
 }
 
 function showZodiacInformation(animalName) {
-	let $information = $('.information');
+	const $information = $('.information');
 	
-	$information.find('h2').text(animalName);
+	$information.find('.zodiac-animal').text(animalName);
 	
-	let animalInformation = getAnimalInformation(animalName);
+	const animalInformation = getAnimalInformation(animalName);
 	$information.find('img').attr('src', animalInformation['image']);
-	$information.find('p').text(animalInformation['description']);
+	$information.find('.personality').text(animalInformation['description']);
 }
 
-$('.yearOfBirth').submit(function(event){
+$('.year-of-birth').submit(function(e){
 			
-	let chosenYear = $('.yearValue').val();
-	let animalName = getAnimalName(chosenYear);
+	const chosenYear = $('.year-value').val();
+	const animalName = getAnimalName(chosenYear);
 	console.log(chosenYear);
 	console.log(getAnimalName(chosenYear));
 	showZodiacInformation(animalName);
 	
-	event.preventDefault();
+	e.preventDefault();
 });
